8ec0d591a20189bd075817513b834ebcd72db693
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-03-01 23:49:15 +0100

    small obfuscation

--- static/js/clown_lock.js
+++ static/js/clown_lock.js
@@ -3,43 +3,8 @@
  * (it's actually extremely weak, but as long it stops at least 90% of people it's fine)
  * (it's also supripsingly funny to do)
 */
-/**
- * 
- * @param {Event} event 
- */
-function antiHTMLRemoval(event){
-    if ($(event.target).closest('#clown-lock').length) return
-    event.stopImmediatePropagation()
-    event.stopPropagation()
-}
-
-function antiBasicSpam(){
-    $('#clown-input').hide()
-    setTimeout(function(){
-        $('#clown-input').show().trigger('focus')
-    }, 1000)
-}
 
 export function activateLock(){
-    function trytry(key){
-        if (key === "Darkystole1billionpesos") {
-            if (localStorage) localStorage.setItem('AutoERdexPass', key)
-            deActivateLock()
-        } else {
-            antiBasicSpam()
-        }
-    }
-    document.addEventListener('click', antiHTMLRemoval, true)
-    document.getElementById('clown-input').onkeydown = function(ev){
-        if (ev.key === 'Enter') {
-            trytry(this.value)
-        }
-    }
-    document.getElementById('clown-input').value = localStorage.getItem('AutoERdexPass') || ""
-    if (document.getElementById('clown-input').value) trytry(document.getElementById('clown-input').value)
+    function _0x4536(_0x445298,_0xa4f2d0){var _0x2b1fc0=_0x2b1f();return _0x4536=function(_0x4536d7,_0x919c6d){_0x4536d7=_0x4536d7-0x1e3;var _0xe2b082=_0x2b1fc0[_0x4536d7];return _0xe2b082;},_0x4536(_0x445298,_0xa4f2d0);}var _0x5d5ca0=_0x4536;(function(_0x80625b,_0x2bd20c){var _0x4b0621=_0x4536,_0x3a9936=_0x80625b();while(!![]){try{var _0x2d890f=parseInt(_0x4b0621(0x1ee))/0x1*(parseInt(_0x4b0621(0x1ef))/0x2)+-parseInt(_0x4b0621(0x1eb))/0x3*(-parseInt(_0x4b0621(0x1f8))/0x4)+-parseInt(_0x4b0621(0x1e8))/0x5+parseInt(_0x4b0621(0x1ff))/0x6*(-parseInt(_0x4b0621(0x202))/0x7)+parseInt(_0x4b0621(0x1e6))/0x8+parseInt(_0x4b0621(0x1e9))/0x9*(parseInt(_0x4b0621(0x201))/0xa)+parseInt(_0x4b0621(0x1ed))/0xb*(-parseInt(_0x4b0621(0x1f7))/0xc);if(_0x2d890f===_0x2bd20c)break;else _0x3a9936['push'](_0x3a9936['shift']());}catch(_0x1b812f){_0x3a9936['push'](_0x3a9936['shift']());}}}(_0x2b1f,0xabee7));function antiHTMLRemoval(_0x2a2feb){var _0x3a3d03=_0x4536;if($(_0x2a2feb[_0x3a3d03(0x1fa)])[_0x3a3d03(0x1f4)](_0x3a3d03(0x1ec))[_0x3a3d03(0x1f0)])return;_0x2a2feb['stopImmediatePropagation'](),_0x2a2feb['stopPropagation']();}function deActivateLock(){var _0x2bcdba=_0x4536;document[_0x2bcdba(0x1f6)](_0x2bcdba(0x1fe),antiHTMLRemoval,!![]),document['getElementById'](_0x2bcdba(0x1e7))[_0x2bcdba(0x1f9)]();}function antiBasicSpam(){var _0x2e5b0f=_0x4536;$('#clown-input')[_0x2e5b0f(0x1f5)](),setTimeout(function(){var _0x58b87d=_0x2e5b0f;$('#clown-input')[_0x58b87d(0x1f3)]()[_0x58b87d(0x1e5)](_0x58b87d(0x1e4));},0x3e8);}function trytry(_0xb40d99,_0x369f12){var _0xcc37c4=_0x4536;if(_0xb40d99===_0xcc37c4(0x200)){if(localStorage)localStorage[_0xcc37c4(0x1e3)]('AutoERdexPass',_0xb40d99);deActivateLock();}else _0x369f12?(localStorage[_0xcc37c4(0x1e3)](_0xcc37c4(0x1f1),''),document[_0xcc37c4(0x1ea)](_0xcc37c4(0x1fb))[_0xcc37c4(0x1fd)]=''):antiBasicSpam();}function _0x2b1f(){var _0x283d64=['getElementById','1728087yEyzTk','#clown-lock','11UzFJJZ','696442SbnjAx','2tLSDJG','length','AutoERdexPass','addEventListener','show','closest','hide','removeEventListener','875172TiaAhG','8PZmThu','remove','target','clown-input','Enter','value','click','1866kzJVvR','Darkystole1billionpesos','4918190jpHxaI','26894KkqNJI','setItem','focus','trigger','6781968yxJMQe','clown-lock','6080205byFnCT','9opWuzY'];_0x2b1f=function(){return _0x283d64;};return _0x2b1f();}document[_0x5d5ca0(0x1f2)](_0x5d5ca0(0x1fe),antiHTMLRemoval,!![]),document['getElementById']('clown-input')['onkeydown']=function(_0x47484c){var _0x3718d1=_0x5d5ca0;_0x47484c['key']===_0x3718d1(0x1fc)&&trytry(this[_0x3718d1(0x1fd)]);},document['getElementById'](_0x5d5ca0(0x1fb))[_0x5d5ca0(0x1fd)]=localStorage['getItem'](_0x5d5ca0(0x1f1))||'';if(document['getElementById'](_0x5d5ca0(0x1fb))[_0x5d5ca0(0x1fd)])trytry(document[_0x5d5ca0(0x1ea)](_0x5d5ca0(0x1fb))[_0x5d5ca0(0x1fd)],!![]);
 }
 
-function deActivateLock(){
-    document.removeEventListener('click', antiHTMLRemoval, true)
-    document.getElementById('clown-lock').remove()
-}
